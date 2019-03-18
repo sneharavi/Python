043a4a142e6658d924ea48b682ef8f013ea26d1d
Sneha Ravi Chandran <sneharavi03@gmail.com>
2019-03-18 17:41:06 -0500

    Updated Code.

--- assignment3/prog3.py
+++ assignment3/prog3.py
@@ -3,35 +3,56 @@
 from time import time
 from random import seed, randint
 
+def printBoard(board):
+   print(board)
+
 def isSafe(board, row, col): 
-# Check this row on left side 
-   for i in range(col): 
-      if board[row][i] == 1: 
+# Check this row on left side
+   for i in range(col):
+      if board[row][i] == 1:
          return False
 
-# Check upper diagonal on left side 
-   for i,j in zip(range(row,-1,-1), range(col,-1,-1)): 
-      if board[i][j] == 1: 
+# Check upper diagonal on left side
+   for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+      if board[i][j] == 1:
          return False
 
-# Check lower diagonal on left side 
-   for i,j in zip(range(row,N,1), range(col,-1,-1)): 
-      if board[i][j] == 1: 
+# Check lower diagonal on left side
+   for i, j in zip(range(row, len(board[0]), 1), range(col, -1, -1)):
+      if board[i][j] == 1:
          return False
 
    return True
+def solveNQUtil(board,row):
+   if row >= len(board[0]):
+      return True
+   for i in range(len(board[0])):
+      if isSafe(board, row, i):
+         board[row][i] = 1
+         if solveNQUtil(board, row+1):
+            return True
+         board[row][i] = 0
+   return False
 
-def solveNQueens (Data):
-   return None
+def solveNQueens (N):
+   boardData = InitBoard(N)
+   if solveNQUtil(boardData, 0) == False:
+      print("Solution does not exist")
+      return False
+   printBoard(boardData)
+   return True
 
 def InitBoard(N):
-""" Initialize board."""
-   boardArray  = [[0 for col in range(N)] for row in range(N)]
+   seed(a=None, version=2)
+   row = randint(0, N)
+   column = randint(0, N)
+   boardArray = [[0 for col in range(N)] for row in range(N)]
+   boardArray[row][column] = 1
    return boardArray
 
 def driver():
    for N  in range(1, 9):
-      solveNQueens(InitBoard(N))
+      solveNQueens(N)
 
 def main():
    """Main function"""
@@ -39,3 +60,4 @@ def main():
 
 if __name__ == '__main__':
    main()
+   
