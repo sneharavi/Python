9c857b173ce79ffcb1495604ef21f84580ecf2c7
Sneha Ravi Chandran <sneharavi03@gmail.com>
2019-03-20 13:14:37 -0500

    Fixed all issues with code.

--- assignment3/prog3.py
+++ assignment3/prog3.py
@@ -39,34 +39,38 @@ def isSafe(board, row, col):
    for i in range(len(board[0])):
       for j in range(len(board[0])):
          if abs(row-i) == abs(col-j):
-            if row != i and col != j:
+            if i != row and j != col:
                if board[i][j]:
                   return False
    return True
- 
+
 def solveNQUtil(board,row):
    if row >= len(board[0]):
       return True
-   for i in range(len(board[0])):
-      col = randint(0, len(board[0])-1)
+   col = randint(0, len(board)-1)
+   for i in range(len(board)):
       if isSafe(board, row, col):
          board[row][col] = True
          if solveNQUtil(board, row+1):
             return True
-         else:
-            board[row][col] = False
+         board[row][col] = False
+      if isSafe(board, row, i):
+         board[row][i] = True
+         if solveNQUtil(board, row+1):
+            return True
+         board[row][i] = False
    return False
 
 def solveNQueens(N):
    boardData = InitBoard(N)
    if solveNQUtil(boardData, 0):
       printBoard(boardData)
+      return True
    else:
       print("Solution does not exist")
-   return
 
 def InitBoard(N):
-   seed(a=None, version=2)
+   seed(a=time(), version=2)
    boardArray = [[False for col in range(N)] for row in range(N)]
    return boardArray
 
@@ -81,4 +85,3 @@ def main():
 
 if __name__ == '__main__':
    main()
-   
